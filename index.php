21928e99ba5310a973287aff50a17997e87a9a2f
linlongzhou <lin_longzhou@qq.com>
2016-11-04 14:14:43 +0800

    mysql test

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+$con = mysql_connect("db0.daocloudinternal.io:60277","root","SS3EO1YH1");
+if (!$con){
+  die('Could not connect: ' . mysql_error());
+}else{
+	echo "mysql connected!";
+}
 echo "Hello Docker";
+mysql_close($con);
+
 ?>
