b32f21759ea51ee1be5d565b587ec5bf593940d8
linlongzhou <lin_longzhou@qq.com>
2016-11-04 15:12:02 +0800

    update

--- index.php
+++ index.php
@@ -3,7 +3,13 @@ $con = mysql_connect("db0.daocloudinternal.io:60277","root","SS3EO1YH1");
 if (!$con){
   die('Could not connect: ' . mysql_error());
 }else{
-	echo "mysql connected!";
+	mysql_select_db("dragon", $con);
+	$result = mysql_query("SELECT * FROM user");
+  	while($row = mysql_fetch_array($result))
+  	{
+  		var_dump($row);
+  		echo "<br />";
+  	}
 }
 echo "Hello Docker";
 mysql_close($con);
